717d7f0ef81afe4bdf7495ae019fc287952f36be
rafaelmatheus <rafaelmatheusdecastro@hotmail.com.br>
2019-03-31 01:01:13 -0300

    Construindo tela de listar clientes

---
+++ src/app/entity/cliente.entity.ts
@@ -0,0 +1,8 @@
+import { EventManagerPlugin } from '@angular/platform-browser/src/dom/events/event_manager';
+
+export interface clienteEntity{
+    matricula: string,
+    nome: string,
+    dataNascimento: Date,
+    email: string
+}
--- src/app/pages/listar-cliente/listar-cliente.component.ts
+++ src/app/pages/listar-cliente/listar-cliente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { clienteEntity } from 'src/app/entity/cliente.entity';
 
 @Component({
   selector: 'app-listar-cliente',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-cliente.component.css']
 })
 export class ListarClienteComponent implements OnInit {
-
+  public clientes: clienteEntity[] = [{
+    matricula:'213131',
+    dataNascimento: new Date(),
+    email: 'rafaelmatheusdecastro@hotmail.com.br',
+    nome: 'rafael'
+  },
+  {
+    matricula:'323311331',
+    dataNascimento: new Date(),
+    email: 'rafaelmatheusdecastro@hotmail.com.br',
+    nome: 'rafael'
+  }
+  
+];
   constructor() { }
 
   ngOnInit() {
