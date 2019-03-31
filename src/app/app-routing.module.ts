896c5b58b0fdd3e1ed24221904f6756409754ccb
rafaelmatheus <rafaelmatheusdecastro@hotmail.com.br>
2019-03-31 00:41:49 -0300

    Estrutura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListarClienteComponent } from './pages/listar-cliente/listar-cliente.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', component: ListarClienteComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarClienteComponent } from './pages/listar-cliente/listar-cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarClienteComponent
   ],
   imports: [
     BrowserModule,
