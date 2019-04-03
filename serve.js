0bb251592f934d6ecd477a5175e208d9db36ec83
rafaelmatheus <rafaelmatheusdecastro@hotmail.com.br>
2019-04-03 18:47:47 -0300

    Nova tentativa

--- serve.js
+++ serve.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/CRUD-FRONTEND'));
+app.use(express.static(__dirname + '/dist/crud-frontend'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/CRUD-FRONTEND/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/crud-frontend/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
