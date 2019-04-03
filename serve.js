653b1f97db3528be384f26535dfe9ffc8558dc91
rafaelmatheus <rafaelmatheusdecastro@hotmail.com.br>
2019-04-03 18:32:36 -0300

    Inicializado para fazer o deploy

--- serve.js
+++ serve.js
@@ -5,10 +5,8 @@ const app = express();
 // Serve static files
 app.use(express.static(__dirname + '/dist/CRUD-FRONTEND'));
 
-// Send all requests to index.html
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/CRUD-FRONTEND/index.html'));
 });
 
-// default Heroku port
 app.listen(process.env.PORT || 5000);
